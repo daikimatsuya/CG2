12c0338888e2350f2926acf4c5a0f990eff513d1
k022g0055 <k022g0055@g.neec.ac.jp>
2025-01-05 08:11:06 +0900

    0_03まで終了

--- main.cpp
+++ main.cpp
@@ -1,7 +1,114 @@
 #include <Windows.h>
+#include <cstdint>
 
-//Windows�A�v���o�̃G���g���[�|�C���g(main�֐�)
+#pragma warning(push)
+//C4023の警告を見なかったことにする
+#pragma warning(disable:4023)
+
+#pragma warning(pop)
+
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WindowProc(HWND hwnd,UINT msg,WPARAM wparam,LPARAM lparam) {
+	//メッセセージに応じてゲーム固有の処理を行う
+	switch (msg) {
+		//ウィンドウが破棄された
+	case WM_DESTROY:
+		//OSにたいして、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	//標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+
+
+#pragma warning(push)
+//C28251の警告を見なかったことにする
+#pragma warning(disable:28251)
+//Windowsアプリ出のエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-		//�o�̓E�B���h�E�ւ̕����o��
+	
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名(なんでもいい)
+	wc.lpszClassName = L"CG2WindowsClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラスを登録する
+	RegisterClass(&wc);
+	
+	///////////////////////////////////////////////////////////////////////////////
+
+	//クライアント領域のサイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	//ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+	//クライアント領域を元に実際のサイズにwrcを変更してもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	///////////////////////////////////////////////////////////////////////////////
+
+	//ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,					//利用するクラス名
+		L"CG2",									//タイトルバーの文字(何でもいい)
+		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
+		CW_USEDEFAULT,					//表示ｘ座標(Windowsに任せる)
+		CW_USEDEFAULT,					//表示ｙ座標(WindowsOSに任せる)
+		wrc.right - wrc.left,					//ウィンドウ横幅
+		wrc.bottom - wrc.top,				//ウィンドウ縦幅
+		nullptr,									//親ウィンドウハンドル
+		nullptr,									//メニューハンドル
+		wc.hInstance,						//インスタンスハンドル
+		nullptr);									//オプション
+
+	//ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+	//////////////////////////////////////////////////////////////////////////////
+
+	MSG msg{};
+	//ウィンドウのＸボタンが押されるまでループ
+	while (msg.message != WM_QUIT){
+		//Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+			//ゲームの処理////////////////////////////////////////////////////////////////////////
+
+			//////////////////////////////////////////////////////////////////////////////////////
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//出力ウィンドウへの文字出力
 	OutputDebugStringA("Hello,DirectX!\n");
 }
+#pragma warning(pop)
